783731bc1c778784ff340e69022fa4ad31d142df
Payam Yektamaram <payamyek@gmail.com>
2025-02-25 21:09:59 -0500

    feat: basic praw skeleton

---
+++ karma-farmer/config.py
@@ -0,0 +1,11 @@
+import os
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    client_id = os.environ["KARMA_FARMER_CLIENT_ID"]
+    client_secret = os.environ["KARMA_FARMER_CLIENT_SECRET"]
+
+
+config = Config()
